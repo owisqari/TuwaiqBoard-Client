0c85e300020056b838216ad3875ba90b4cbc6412
Arwaaa9 <dr.arwa8@gmail.com>
2023-06-19 20:18:22 +0300

    sideBar & homeWork page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 // import { useState } from 'react'
+// eslint-disable-next-line no-unused-vars
 import './App.css'
 // import './index.css'
-import Login from '../pages/login'
-
+import Login from './pages/login'
+import Sidebar from './comp/sidebar'
 function App() {
 
   return (
     <>
     <Login/>
-    
+    <Sidebar/>
     </>
   )
 }
---
+++ src/pages/Annauncements.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Sidebar from './comp/sidebar'
+
+function Annauncements() {
+  return (
+    <>
+    <Sidebar/>
+    <div>Annauncements</div>
+    </>
+  )
+}
+
+export default Annauncements
